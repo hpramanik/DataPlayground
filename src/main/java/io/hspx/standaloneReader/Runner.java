3703a0c1e94d865c65eec80b46dec9d6b2a1876c
hpramanik <hpramanik@outlook.in>
2021-08-17 00:51:28 +0530

    Refactor

--- src/main/java/io/hspx/standaloneReader/Runner.java
+++ src/main/java/io/hspx/standaloneReader/Runner.java
@@ -4,10 +4,8 @@ import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.log4j.BasicConfigurator;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.avro.AvroParquetWriter;
-import org.apache.parquet.example.data.simple.Int96Value;
 import org.apache.parquet.example.data.simple.NanoTime;
 import org.apache.parquet.hadoop.ParquetFileWriter;
 import org.apache.parquet.hadoop.ParquetReader;
@@ -21,29 +19,36 @@ import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Runner {
     public static void main(String[] args) throws IOException {
-//        BasicConfigurator.configure();
         Configuration conf = new Configuration();
         conf.setBoolean(org.apache.parquet.avro.AvroReadSupport.READ_INT96_AS_FIXED, true);
 
-        java.nio.file.Path inputDir = java.nio.file.Path.of("sample-data/parquet/");
+        java.nio.file.Path inputDir = java.nio.file.Path.of("sample-data/parquet/userdata1.parquet");
         java.nio.file.Path outputDir = java.nio.file.Path.of("sample-data/parquet-out/");
 
-        DirectoryStream<java.nio.file.Path> stream = Files.newDirectoryStream(inputDir);
+        if (Files.isDirectory(inputDir)) {
+            DirectoryStream<java.nio.file.Path> stream = Files.newDirectoryStream(inputDir);
 
-        for (java.nio.file.Path path : stream) {
-            if (!Files.isDirectory(path)) {
-                var out = readParquet(conf, path.toString());
-                writeParquet(conf, out, outputDir + "/" + path.getFileName());
+            for (java.nio.file.Path path : stream) {
+                if (!Files.isDirectory(path)) {
+                    var out = readParquet(conf, path.toString());
+                    writeParquet(conf, out, outputDir + "/" + path.getFileName());
+                }
             }
+        } else {
+            var out = readParquet(conf, inputDir.toString());
+            writeParquet(conf, out, outputDir + "/" + inputDir.getFileName());
         }
 
-        stream = Files.newDirectoryStream(outputDir);
+        DirectoryStream<java.nio.file.Path> stream = Files.newDirectoryStream(outputDir);
         for (java.nio.file.Path path : stream) {
             if (!Files.isDirectory(path) && path.getFileName().toString().endsWith(".crc")) {
                 Files.delete(path);
@@ -68,6 +73,7 @@ public class Runner {
         System.out.println(String.format("Reading %s", pathOfFile));
         ParquetReader<GenericData.Record> reader = null;
         Path path = new Path(pathOfFile);
+
         reader = AvroParquetReader
                 .<GenericData.Record>builder(path)
                 .withConf(conf)
@@ -144,14 +150,17 @@ public class Runner {
         }
     }
 
-    public static long getDateTimeValueFromBinary(GenericData.Fixed binaryTimeStampValue) {
-        // This method represents binaryTimeStampValue as ByteBuffer, where timestamp is stored as sum of
-        // julian day number (32-bit) and nanos of day (64-bit)
-
+    public static LocalDateTime getDateTimeValueFromBinary(GenericData.Fixed binaryTimeStampValue) {
         NanoTime nt = NanoTime.fromBinary(Binary.fromConstantByteArray(binaryTimeStampValue.bytes()));
         int julianDay = nt.getJulianDay();
         long nanosOfDay = nt.getTimeOfDayNanos();
-        return (julianDay - 2440588) * TimeUnit.HOURS.toMillis(24)
+
+        var epoch = (julianDay - 2440588) * TimeUnit.HOURS.toMillis(24)
                 + nanosOfDay / TimeUnit.MILLISECONDS.toNanos(1);
+
+        System.out.println(epoch);
+
+        LocalDateTime date= Instant.ofEpochMilli(epoch).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        return date;
     }
 }
